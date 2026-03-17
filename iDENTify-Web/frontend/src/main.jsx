7eb0ccb511a982bee5fdcc4b8a76f186198434a5
erica <ericaaquino0114@gmail.com>
2026-03-18 00:15:04 +0800

    fix ui

--- iDENTify-Web/frontend/src/main.jsx
+++ iDENTify-Web/frontend/src/main.jsx
@@ -1,29 +1,16 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { BrowserRouter } from "react-router-dom";
-// import App from "./App.jsx";
-// import ErrorBoundary from "./ErrorBoundary.jsx";
-// import "./styles/base.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import ErrorBoundary from "./ErrorBoundary.jsx";
+import "./styles/base.css";
 
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//     <BrowserRouter >
-//       <ErrorBoundary>
-//         <App />
-//       </ErrorBoundary>
-//     </BrowserRouter>
-// );
-
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
